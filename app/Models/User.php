d0d37f375c4d3cc8457da29257e8395fc6e4c45c
nipenmajumder <nipenmajumder@users.noreply.github.com>
2023-12-21 09:44:16 +0000

    PHP Linting (Pint)

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,21 +17,21 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = array (
+    protected $fillable = [
         'name',
         'email',
         'password',
-    );
+    ];
 
     /**
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
      */
-    protected $hidden = array (
+    protected $hidden = [
         'password',
         'remember_token',
-    );
+    ];
 
     /**
      * The attributes that should be cast.
